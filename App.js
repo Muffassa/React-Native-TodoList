7da5961acd4563938b5c153330ef423f5221958e
Nikita <keddofilatov@gmail.com>
2017-07-23 13:46:30 +0600

    [+] todo list

--- App.js
+++ App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import { StyleSheet, Platform, Text, TextInput, View } from 'react-native';
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  TouchableHighlight
+} from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       text: '',
+      todos: ['Todo1', 'Todo2', 'Todo3', 'Todo4', 'Todo5']
     };
   }
 
+  openTodo = () => {
+    alert('Open Todo');
+  }
+
   render() {
+    const todoItems = this.state.todos.map((todo, index) => {
+      return (
+        <TouchableHighlight key={index} style={styles.todoItem} onPress={this.openTodo}>
+          <Text>
+            {todo}
+          </Text>
+        </TouchableHighlight>
+      )
+    });
+
     return (
       <View>
         <TextInput
           style={styles.textInput}
           value={this.state.text}
-          onChangeText={(text) => this.setState({text})}
+          onChangeText={(text) => this.setState({ text })}
           placeholder={'Write you TODO'}
         />
         <Text>
           {this.state.text}
         </Text>
+        <ScrollView style={styles.todoItemsList}>
+          {todoItems}
+        </ScrollView>
       </View>
     );
   }
@@ -43,5 +69,14 @@ const styles = StyleSheet.create({
         borderWidth: 1
       },
     })
+  },
+  todoItem: {
+    height: 60,
+    paddingLeft: 20,
+    justifyContent: 'center',
+    marginTop: 2
+  },
+  todoItemsList: {
+    marginTop: 20,
   }
 });
